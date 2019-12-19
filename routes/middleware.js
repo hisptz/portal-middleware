f5b28ed56ac06d403fd2f0ade86bf11f5abb5278
josephatJ <josephatjulius24@gmail.com>
2019-12-19 11:38:06 +0300

    merge middleware codes to include lookup

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,95 +1,102 @@
-var express = require('express');
-var router = express.Router();
-const cheerio = require('cheerio');
-/**
- * TODO: Server address and credentials should be configured in configuration file
-*/
-var URL = 'url';
-var credentials = 'username:password';
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const configurationsRaw = fs.readFileSync("config.json");
+const configurations = JSON.parse(configurationsRaw);
+const request = require("request");
 
-var request = require('request');
+const lookup = {};
 
 // route with regular Expression to escape some characters
 router.get(/\/(\d*)\/?(edit)?/, function(req, res, next) {
-    // define headers
-    var headers = {
-        'Content-Type': 'application/json',
-        "Authorization": 'Basic ' + new Buffer.from(credentials).toString('base64')
-    }
+  const middlewareKey = req.url.split("/")[1];
+  const baseUrl = configurations["instance"];
+  const credentials =
+    configurations["username"] + ":" + configurations["password"];
+  // define headers
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Basic " + new Buffer.from(credentials).toString("base64")
+  };
 
-    // replace portal-middleware with the path of here your portal reference
-    /**
-     * *TODO: put this in the configuration file
-     */
-    var path = req.url.replace('/middleware','');
-    // see the path
-    console.log('PATH middleware', path);
-    if (path.indexOf('extract') > 0) {
-        console.log('extract');
-        var urlExtract = 'http://nacp.go.tz/site/news/HSHSPIIIcostingreportFINAL.json';
-        var Promise = require('promise');
-        var promise = new Promise(function (resolve, reject) {
-            var headers = {
-                'Content-Type':  'application/text',
-                "Authorization": 'Basic ' + new Buffer.from(credentials).toString('base64')
+  var path = req.url.replace("/" + middlewareKey, "");
+  const Promise = require("promise");
+
+  const uri = baseUrl + path;
+  const availableContent = lookup[uri];
+  const promise = availableContent
+    ? new Promise(function(resolve, reject) {
+        resolve(availableContent);
+      })
+    : new Promise(function(resolve, reject) {
+        request(
+          {
+            headers: headers,
+            uri,
+            method: "GET"
+          },
+          function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              const receivedContent = JSON.parse(body);
+              lookup[uri] = receivedContent;
+              resolve(receivedContent);
+            } else {
+              if (response) {
+                console.log(response.statusCode + ":", JSON.stringify(error));
+                reject();
+              } else {
+                console.log(response);
+              }
             }
-            request({
-                    headers: headers,
-                    uri: urlExtract,
-                    method: 'GET'
-                },
-                function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        $=cheerio.load(body);
-                        $('a').each(function (index, val) {
-                            $(val).attr('target', '_blank');
-                            $(val).attr('style', 'color: #2A8FD1; font-size: 0.8em;');
-                        });
-                        $('.panel-success').each(function (index, successDom) {
-                            $(successDom).replaceWith('');
-                        });
-                        $('.panel-title').replaceWith('<h3 style="text-align: left; margin-top: 20px; font-weight: 500; font-size: 1.4em;">More news from NACP news page</h3>')
-                        var extractedData = $('.main-content .col-lg-8').replaceWith('').removeAttr('class');
-                        var data = {
-                            "data": extractedData.toString()
-                        }
-                        resolve(data);
-                    } else {
-                        if (response) {
-                            console.log(error);
-                            reject();
-                        } else {
-                            console.log(response);
-                        }
-                    }
-                })
-        });
-    } else {
-        var Promise = require('promise');
-        var promise = new Promise(function (resolve, reject) {
-            request({
-                    headers: headers,
-                    uri: URL + path,
-                    method: 'GET'
-                },
-                function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        resolve(JSON.parse(body));
-                    } else {
-                        if (response) {
-                            console.log(response.statusCode + ":", JSON.stringify(error));
-                            reject();
-                        } else {
-                            console.log(response);
-                        }
-                    }
-                })
-        });
-    }
-// send when the results are completely loaded
-    promise.then(function(result) {
-        res.send(result);
-    });
+          }
+        );
+      });
+  // send when the results are completely loaded
+  promise.then(function(result) {
+    res.send(result);
+  });
 });
 
+router.put(/\/(\d*)\/?(edit)?/, function(req, res, next) {
+  const data = req.body;
+  const middlewareKey = req.url.split("/")[1];
+  const instanceType = req.url.split("/")[2];
+  const baseUrl = configurations[instanceType]["instance"];
+  const credentials =
+    configurations[instanceType]["username"] +
+    ":" +
+    configurations[instanceType]["password"];
+  const path = req.url
+    .replace("/" + middlewareKey, "")
+    .replace("/" + instanceType, "");
+  // define headers
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Basic " + new Buffer.from(credentials).toString("base64")
+  };
+
+  const Promise = require("promise");
+  const promise = new Promise(function(resolve, reject) {
+    request(
+      {
+        headers: headers,
+        url: baseUrl + path,
+        method: "PUT",
+        json: true,
+        body: data
+      },
+      (err, res, body) => {
+        if (!err) {
+          resolve(body);
+        } else {
+          reject(err);
+        }
+      }
+    );
+  });
+
+  promise.then(function(result) {
+    res.send(result);
+  });
+});
 module.exports = router;
