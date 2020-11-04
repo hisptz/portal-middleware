82c2a993d9929dd3eccfbc32caa58b6a10a7cdff
josephatJ <josephatjulius24@gmail.com>
2020-11-04 19:45:06 +0300

    add support for proxying into afyacare (addressing security certificate issues)

--- routes/middleware.js
+++ routes/middleware.js
@@ -9,6 +9,7 @@ const lookup = {};
 
 // route with regular Expression to escape some characters
 router.get(/\/(\d*)\/?(edit)?/, function(req, res, next) {
+  console.log(req.url.split("/"))
   const middlewareKey = req.url.split("/")[1];
   const baseUrl = configurations["instance"];
   const credentials =
@@ -19,7 +20,8 @@ router.get(/\/(\d*)\/?(edit)?/, function(req, res, next) {
     Authorization: "Basic " + new Buffer.from(credentials).toString("base64")
   };
 
-  var path = req.url.replace("/portal-middleware-live", "");
+  var path = req.url.replace("/middleware", "");
+  console.log(path)
   const Promise = require("promise");
 
   const uri = baseUrl + path;
@@ -31,21 +33,25 @@ router.get(/\/(\d*)\/?(edit)?/, function(req, res, next) {
     : new Promise(function(resolve, reject) {
         request(
           {
+            "rejectUnauthorized": false,
             headers: headers,
             uri,
             method: "GET"
           },
           function(error, response, body) {
+            console.log("here", JSON.stringify(body))
             if (!error && response.statusCode == 200) {
               const receivedContent = JSON.parse(body);
-              lookup[uri] = receivedContent;
+              // console.log(receivedContent)
+              // lookup[uri] = receivedContent;
               resolve(receivedContent);
             } else {
+              // console.log('response', response)
               if (response) {
-                console.log(response.statusCode + ":", JSON.stringify(error));
+                // console.log(response.statusCode + ":", JSON.stringify(error));
                 reject();
               } else {
-                console.log(response);
+                // console.log(response);
               }
             }
           }
