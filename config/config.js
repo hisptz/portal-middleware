c8b616e59577af04df4cbcfefeae78b027f1c046
Bennett Benedict <bennybenester@gmail.com>
2022-08-22 10:32:29 +0300

    release: change instance name

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const configurations = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
-  instance: process.env.DHIS2_URL,
+  instance: process.env.INSTANCE,
 };
 
 module.exports = configurations;
