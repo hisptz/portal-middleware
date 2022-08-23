001f115268a0dec2168287427a0cdaf60ceb943b
Bennett Benedict <bennybenester@gmail.com>
2022-08-23 13:29:41 +0300

    release: replace api/me request

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,7 +19,7 @@ router.get(/\/(\d*)\/?(edit)?/, (req, res, next) => {
   var path = req.url.replace(configurations.replaceKey, "");
 
   if (path.includes("/api/me?")) {
-    path = "/api/users?filter=username:eq:" + configurations.username;
+    path = "/api/users.json/" + configurations.id;
   }
 
   const uri = baseUrl + path;
